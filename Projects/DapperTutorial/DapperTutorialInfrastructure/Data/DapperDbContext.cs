12317aa4eee5244db6f1a186e34c40b0f9a50d06
zhaojiawei86 <59852184+zhaojiawei86@users.noreply.github.com>
2023-02-15 20:56:44 -0500

    Add files via upload

---
+++ Projects/DapperTutorial/DapperTutorialInfrastructure/Data/DapperDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DapperTutorialInfrastructure.Data
+{
+	public class DapperDbContext
+	{
+        IDbConnection dbConnection;
+		public DapperDbContext()
+		{
+
+            var connectionString = "data source=localhost;initial catalog=JanBatch2023;user id=sa;password=SGF86&pop";
+            dbConnection = new SqlConnection(connectionString);
+        }
+
+		public IDbConnection GetConnection()
+		{
+			return dbConnection;
+		}
+	}
+}
+
